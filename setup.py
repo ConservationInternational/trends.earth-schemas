f6be3a9c7133b648ce0084a53bb124a3eb7a275e
Alex Zvoleff <azvoleff@gmail.com>
2018-01-22 16:34:56 +0100

    Fix paver to set versions.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3',
+    version='0.1',
 
     description='Stores the schemas used internally in the trends.earth package to handle metadata for analysis results.',
     long_description=long_description,
