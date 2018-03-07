254a812cb700e1b1ad494d560dd39d5ffab34f86
Alex Zvoleff <azvoleff@gmail.com>
2018-03-07 03:20:04 -0500

    Improve schemas for offline calculations.

--- schemas.py
+++ schemas.py
@@ -27,7 +27,7 @@ class TimeSeriesTable(object):
 class TimeSeriesTableSchema(Schema):
     type = fields.Str()
     name = fields.Str()
-    table = fields.Nested(TimeSeriesSchema(), many=True)
+    table = fields.Nested(TimeSeriesSchema, many=True)
 
 
 ################################################################################
@@ -46,47 +46,14 @@ class BandInfo(object):
 
 
 class BandInfoSchema(Schema):
-    name = fields.Str()
-    no_data_value = fields.Number()
-    add_to_map = fields.Boolean()
-    metadata = fields.Dict()
+    name = fields.Str(required=True)
+    no_data_value = fields.Number(required=True)
+    add_to_map = fields.Boolean(required=True)
+    metadata = fields.Dict(required=True)
 
 
 ################################################################################
-# Schema for local analyses
-
-class File(object):
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-
-class FileSchema(Schema):
-    file_name = fields.Str()
-
-
-class LocalResults(object):
-    def __init__(self, name, bands, files, script_version, metadata, file_format='tif'):
-        self.type = "LocalResults"
-        self.name = name
-        self.bands = bands
-        self.files = files
-        self.script_version = script_version
-        self.metadata = metadata
-        self.file_format = file_format
-
-
-class LocalResultsSchema(Schema):
-    type = fields.Str()
-    name = fields.Str()
-    bands = fields.Nested(BandInfoSchema(), many=True)
-    files = fields.Nested(FileSchema(), many=True)
-    script_version = fields.Str()
-    metadata = fields.Dict()
-    file_format = fields.Dict()
-
-
-################################################################################
-# Schema for downloads
+# Schema for output from locally run calculations
 
 class Url(object):
     def __init__(self, url, md5Hash):
@@ -95,20 +62,48 @@ class Url(object):
 
 
 class UrlSchema(Schema):
-    url = fields.Url()
+    url = fields.Url(required=True)
     md5Hash = fields.Str()
 
 
-class CloudResults(object):
-    def __init__(self, name, bands, urls):
-        self.type = "CloudResults"
-        self.name = name
+################################################################################
+# Schema for responses from api.trends.earth
+
+class APIResponseSchema(Schema):
+    end_date = fields.DateTime(required=True)
+    id = fields.Str(required=True)
+    params = fields.Dict(required=True)
+    progress = fields.Integer()
+    results = fields.Dict(required=True)
+    script = fields.Dict(required=True)
+    script_id = fields.UUID(required=True)
+    start_date = fields.DateTime(required=True)
+    status = fields.Str()
+    user_id = fields.UUID()
+
+
+################################################################################
+# Schema used for all TE results (cloud or local)
+
+class LocalRaster(object):
+    def __init__(self, file, bands, metadata):
+        self.file = file
         self.bands = bands
-        self.urls = urls
+        self.metadata = metadata
 
 
-class CloudResultsSchema(Schema):
-    type = fields.Str()
-    name = fields.Str()
-    bands = fields.Nested(BandInfoSchema(), many=True)
-    urls = fields.Nested(UrlSchema(), many=True)
+class LocalRasterSchema(Schema):
+    file = fields.Str(required=True)
+    bands = fields.Nested(BandInfoSchema, required=True, many=True)
+    metadata = fields.Dict(required=True)
+
+
+class LocalTable(object):
+    def __init__(self, data, metadata):
+        self.data = data
+        self.metadata = metadata
+
+
+class LocalTableSchema(Schema):
+    data = fields.Nested(TimeSeriesTableSchema)
+    metadata = fields.Dict()
