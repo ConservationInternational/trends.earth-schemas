ef5d831e2d6ed8864eef82ebda44ed8140b03ec6
Alex Zvoleff <azvoleff@gmail.com>
2018-11-27 12:06:04 -0500

    Bump marshmallow version.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['marshmallow==3.0.0b7'],
+    install_requires=['marshmallow==3.0.0b20'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
