c61bd18a215676dbb8b94070bd18beb3f141d613
Alex Zvoleff <azvoleff@gmail.com>
2020-03-31 20:46:09 -0400

    Upgrade marshmallow.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['marshmallow==3.0.0b20'],
+    install_requires=['marshmallow==3.3.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
