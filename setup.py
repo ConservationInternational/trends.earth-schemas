7308f464d2b9ddbd5c5b0ce0ca432d668039e020
Alex Zvoleff <azvoleff@gmail.com>
2020-03-31 21:33:57 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.68',
+    version='0.98',
 
     description='Schemas supporting the Trends.Earth QGIS plugin.',
 
