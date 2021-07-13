825915f03460e338582e3e58621f08de8912396f
Alex Zvoleff <azvoleff@conservation.org>
2021-07-13 18:50:24 -0400

    Rename to simplify imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from codecs import open
 from os import path
 
 setup(
-    name='trendsearth-schemas',
+    name='te_schemas',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
