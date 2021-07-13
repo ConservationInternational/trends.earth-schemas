56162244780b7c1cb0bfd7ce6b9f58ec16cb4fa3
Alex Zvoleff <azvoleff@conservation.org>
2021-07-13 18:22:23 -0400

    Fix version spec

--- setup.py
+++ setup.py
@@ -61,8 +61,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['marshmallow==3.11.1',
-                      'marshmallow-dataclass=8.3.2'],
+    install_requires=['marshmallow==3.12.2',
+                      'marshmallow-dataclass==8.4.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
