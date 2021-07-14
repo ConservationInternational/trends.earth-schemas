d183d9b30ff2468c0cc29374270b5255f8ead881
Alex Zvoleff <azvoleff@conservation.org>
2021-07-14 09:04:27 -0400

    Move modules into a separate folder for clarity

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=[]),
+    packages = ['te_schemas'],
+    package_dir = {'te_schemas' : 'te_schemas'},
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
