e53e1d44679f7f6d84a321252550ca686129f9f2
Alex Zvoleff <azvoleff@conservation.org>
2022-05-30 23:41:35 +0200

    Fix syntax

--- te_schemas/results.py
+++ te_schemas/results.py
@@ -6,7 +6,7 @@ import typing
 from dataclasses import field
 
 import marshmallow_dataclass
-from marshmallow import validate
+from marshmallow import validate, EXCLUDE
 from marshmallow_dataclass.typing import Url
 
 from .path import Path
@@ -185,7 +185,7 @@ class RasterResults:
 @marshmallow_dataclass.dataclass
 class EmptyResults:
     class Meta:
-        unknown = 'EXCLUDE'
+        unknown = EXCLUDE
 
     name: typing.Optional[str] = None
     data_path: typing.Optional[Path] = None
@@ -201,7 +201,7 @@ class EmptyResults:
 @marshmallow_dataclass.dataclass
 class CloudResults:
     class Meta:
-        unknown = 'EXCLUDE'
+        unknown = EXCLUDE
 
     name: str
     bands: typing.List[Band]
