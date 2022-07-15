6363756b4adb7fcecbf419019c9675401e3bb8fd
Alex Zvoleff <azvoleff@conservation.org>
2022-07-15 09:07:33 -0400

    Support BUILDING status

--- te_schemas/jobs.py
+++ te_schemas/jobs.py
@@ -13,8 +13,8 @@ from . import SchemaBase
 from .algorithms import ExecutionScript
 from .path import Path
 from .results import EmptyResults
-from .results import JsonResults
 from .results import FileResults
+from .results import JsonResults
 from .results import RasterResults
 from .results import TimeSeriesTableResult
 from .results import VectorResults
@@ -23,6 +23,7 @@ from .results import VectorResults
 class ScriptStatus(enum.Enum):
     SUCCESS = "SUCCESS"
     FAIL = "FAIL"
+    BUILDING = "BUILDING"
 
 
 class JobStatus(enum.Enum):
@@ -54,12 +55,8 @@ class RemoteScript:
 
     @post_load
     def set_timezone(self, data, **kwargs):
-        data['created_at'] = data['created_at'].replace(
-            tzinfo=datetime.timezone.utc
-        )
-        data['updated_at'] = data['updated_at'].replace(
-            tzinfo=datetime.timezone.utc
-        )
+        data["created_at"] = data["created_at"].replace(tzinfo=datetime.timezone.utc)
+        data["updated_at"] = data["updated_at"].replace(tzinfo=datetime.timezone.utc)
 
         return data
 
@@ -67,7 +64,7 @@ class RemoteScript:
 @marshmallow_dataclass.dataclass
 class JobLocalContext:
     base_dir: Path = dataclasses.field(default=None)
-    area_of_interest_name: str = dataclasses.field(default='unknown-area')
+    area_of_interest_name: str = dataclasses.field(default="unknown-area")
 
 
 @marshmallow_dataclass.dataclass
@@ -80,10 +77,16 @@ class Job:
     local_context: typing.Optional[JobLocalContext] = dataclasses.field(
         default_factory=JobLocalContext
     )
-    results: typing.Optional[typing.Union[
-        RasterResults, FileResults, JsonResults, TimeSeriesTableResult,
-        VectorResults, EmptyResults
-    ]] = dataclasses.field(default_factory=dict)
+    results: typing.Optional[
+        typing.Union[
+            RasterResults,
+            FileResults,
+            JsonResults,
+            TimeSeriesTableResult,
+            VectorResults,
+            EmptyResults,
+        ]
+    ] = dataclasses.field(default_factory=dict)
     task_name: typing.Optional[str] = None
     task_notes: typing.Optional[str] = None
     script: typing.Optional[ExecutionScript] = None
@@ -92,41 +95,39 @@ class Job:
 
     @pre_load
     def set_script_name_version(self, data, **kwargs):
-        script_id = data.pop('script_id', None)
-        params_script = data['params'].pop('script', None)
+        script_id = data.pop("script_id", None)
+        params_script = data["params"].pop("script", None)
 
-        if not data.get('script'):
+        if not data.get("script"):
             if params_script:
-                data['script'] = params_script
+                data["script"] = params_script
             elif script_id:
-                data['script'] = ExecutionScript.Schema().dump(
+                data["script"] = ExecutionScript.Schema().dump(
                     ExecutionScript(script_id)
                 )
             else:
-                data['script'] = ExecutionScript.Schema().dump(
+                data["script"] = ExecutionScript.Schema().dump(
                     ExecutionScript("Unknown script")
                 )
 
-        script_name_regex = re.compile(
-            '([0-9a-zA-Z -]*)(?: *)([0-9]+(_[0-9]+)+)'
-        )
-        matches = script_name_regex.search(data['script'].get('name'))
+        script_name_regex = re.compile("([0-9a-zA-Z -]*)(?: *)([0-9]+(_[0-9]+)+)")
+        matches = script_name_regex.search(data["script"].get("name"))
 
         if matches:
-            data['script']['name'] = matches.group(1).rstrip()
-            data['script']['version'] = matches.group(2).replace('_', '.')
+            data["script"]["name"] = matches.group(1).rstrip()
+            data["script"]["version"] = matches.group(2).replace("_", ".")
 
         return data
 
     @pre_load
     def set_main_fields_from_params(self, data, **kwargs):
-        field_names = ['task_name', 'task_notes', 'local_context']
+        field_names = ["task_name", "task_notes", "local_context"]
 
         for field_name in field_names:
             field_value = None
 
-            if field_name in data['params']:
-                field_value = data['params'].pop(field_name)
+            if field_name in data["params"]:
+                field_value = data["params"].pop(field_name)
 
             if not data.get(field_name) and field_value:
                 data[field_name] = field_value
@@ -135,14 +136,10 @@ class Job:
 
     @post_load
     def set_timezone(self, data, **kwargs):
-        data['start_date'] = data['start_date'].replace(
-            tzinfo=datetime.timezone.utc
-        )
+        data["start_date"] = data["start_date"].replace(tzinfo=datetime.timezone.utc)
 
-        if data['end_date']:
-            data['end_date'] = data['end_date'].replace(
-                tzinfo=datetime.timezone.utc
-            )
+        if data["end_date"]:
+            data["end_date"] = data["end_date"].replace(tzinfo=datetime.timezone.utc)
 
         return data
 
@@ -154,14 +151,14 @@ class Job:
             else:
                 script_name = self.script.name
         else:
-            script_name = ''
+            script_name = ""
 
         if self.task_name and script_name:
             name = f"{self.task_name} ({script_name})"
         elif self.task_name:
-            name = self.task_name + ' (unknown script)'
+            name = self.task_name + " (unknown script)"
         elif script_name:
-            name = script_name + ' (unnamed task)'
+            name = script_name + " (unnamed task)"
         else:
             name = "Unnamed task (unknown script)"
 
