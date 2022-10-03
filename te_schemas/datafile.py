191a85ad8117819c06523fa58d87351034e81d89
Alex Zvoleff <azvoleff@conservation.org>
2022-10-03 16:08:38 -0400

    Allow name_filter to be a list

--- te_schemas/datafile.py
+++ te_schemas/datafile.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 import marshmallow_dataclass
 
@@ -12,37 +12,46 @@ class DataFile:
     bands: List[Band]
 
     def indices_for_name(
-        self, name_filter: str, field: str = None, field_filter: str = None
+        self, name_filter: Union[str, list], field: str = None, field_filter: str = None
     ):
+        if isinstance(name_filter, str):
+            # make name_filter a length 1 list if it is a string
+            name_filter = [name_filter]
         if field:
             assert field_filter is not None
 
             return [
                 index
                 for index, band in enumerate(self.bands)
-                if (band.name == name_filter and band.metadata[field] == field_filter)
+                if (band.name in name_filter and band.metadata[field] == field_filter)
             ]
         else:
             return [
                 index
                 for index, band in enumerate(self.bands)
-                if band.name == name_filter
+                if band.name in name_filter
             ]
 
     def index_for_name(
-        self, name_filter: str, field: str = None, field_filter: str = None
+        self, name_filter: Union[str, list], field: str = None, field_filter: str = None
     ):
         """throw an error if more than one result"""
+        if isinstance(name_filter, str):
+            # make name_filter a length 1 list if it is a string
+            name_filter = [name_filter]
         out = self.indices_for_name(name_filter, field, field_filter)
 
         if len(out) > 1:
-            raise RuntimeError(f"more than one band found for name {name_filter}")
+            raise RuntimeError(f"more than one band found for {name_filter}")
         else:
             return out[0]
 
-    def metadata_for_name(self, name_filter: str, field: str):
+    def metadata_for_name(self, name_filter: Union[str, list], field: str):
         """get value of metadata field for all bands of specific type"""
-        m = [b.metadata[field] for b in self.bands if b.name == name_filter]
+        if isinstance(name_filter, str):
+            # make name_filter a length 1 list if it is a string
+            name_filter = [name_filter]
+        m = [b.metadata[field] for b in self.bands if b.name in name_filter]
 
         if len(m) == 1:
             return m[0]
