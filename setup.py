6c07de0ddfda13163a9475da597ea1c079edc8b5
Alex Zvoleff <azvoleff@conservation.org>
2022-11-06 16:31:58 -0500

    Fix for renamed readme

--- setup.py
+++ setup.py
@@ -1,62 +1,40 @@
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="te_schemas",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version="2.0.1",
     description="Schemas supporting the Trends.Earth QGIS plugin.",
-    # The project's main homepage.
+    long_description=long_description,
     url="https://github.com/ConservationInternational/trends.earth-schemas",
-    # Author details
     author="Conservation International",
     author_email="trends.earth@conservation.org",
-    # Choose your license
     license="GPL-2.0",
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 4 - Beta",
-        # Indicate who your project is intended for
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: GIS",
-        # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    # What does your project relate to?
     keywords="land degradation LDN SDG sustainable development goals",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=["te_schemas"],
     package_dir={"te_schemas": "te_schemas"},
     package_data={"te_schemas": ["version.json"]},
     include_package_data=True,
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         "typing-inspect==0.7.1",
         "marshmallow>=3.18.0",
         "marshmallow-dataclass[enum, union]==8.5.9",
     ],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         "dev": ["check-manifest"],
         "test": ["coverage"],
