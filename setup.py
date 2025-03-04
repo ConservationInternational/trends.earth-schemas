3147251c8a333b819dafec0c37c4377f33c24067
Alex Zvoleff <azvoleff@conservation.org>
2025-03-04 15:36:40 -0500

    Drop GDAL for now to avoid issues when packaging trends.earth plugin

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "defusedxml>=0.7.1",
         "marshmallow>=3.21.3",
         "marshmallow-dataclass[enum, union]>=8.7.0",
-        "gdal",
     ],
     extras_require={
         "dev": ["check-manifest"],
