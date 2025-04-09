68f2d96756cea19e9263bec16b3f219d9f1a1c25
Alex Zvoleff <azvoleff@conservation.org>
2025-04-09 14:55:10 -0400

    Add sphinx deps

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setup(
     install_requires=[
         "defusedxml>=0.7.1",
         "marshmallow>=3.21.3",
-        "marshmallow-dataclass[enum, union]>=8.7.0",
+        "marshmallow-dataclass[enum, union]>=8.7.1",
     ],
     extras_require={
-        "dev": ["check-manifest"],
+        "dev": ["check-manifest", "sphinx"],
         "test": ["coverage", "pytest"],
     },
 )
