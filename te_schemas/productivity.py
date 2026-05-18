c051ae3cadcc9ab9d752636a95eb5bc4cf69bfb9
Dimas <dimas.ciputra@gmail.com>
2026-05-18 14:08:23 +0100

    Add FWV2_5_CLASS_LPD to ProductivityMode enum

--- te_schemas/productivity.py
+++ te_schemas/productivity.py
@@ -5,4 +5,5 @@ class ProductivityMode(enum.Enum):
     TRENDS_EARTH_5_CLASS_LPD = "TrendsEarth-LPD-5"
     JRC_5_CLASS_LPD = "JRC-LPD-5"
     FAO_WOCAT_5_CLASS_LPD = "FAO-WOCAT-LPD-5"
+    FWV2_5_CLASS_LPD = "FWv2-LPD-5"
     CUSTOM_5_CLASS_LPD = "Custom-LPD-5"
